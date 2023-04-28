fe6bb10511191d21628aab1c819753f9ce61ce88
navaneeth <navaneethakannan261@gmail.com>
2023-04-28 17:49:26 +0530

    Added Verification and Login History to Home Page

--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -9,6 +9,7 @@ const HomePage = () => {
   const { setAppStatus, userDetails, setUserDetails } = useContext(AppContext);
 
   const [message, setMessage] = useState([]);
+  const [verificationHistory, setVerificationHistory] = useState({});
 
   const loginMessage = [
     `You have been successfully authenticated using JWT token. Your registered mobile number is ${userDetails.phoneNumber}. `,
@@ -19,12 +20,13 @@ const HomePage = () => {
       const response = await get("/api/users/profile");
       if (response?.status) {
         setMessage(response.message);
+        setVerificationHistory(response.data.verificationHistory);
       }
     };
     fetchUser();
   }, []);
 
-  const handleLogout = async() => {
+  const handleLogout = async () => {
     await get("/api/users/logout");
     setUserDetails({ userId: "", phoneNumber: "", name: "" });
     setAppStatus({ isLoading: false, isLoggedIn: false });
@@ -55,6 +57,27 @@ const HomePage = () => {
     window.open(link, "_blank");
   };
 
+  const convertDate = (isoDate) => {
+    if (!isoDate) {
+      return null;
+    }
+    const date = new Date(isoDate);
+    const options = {
+      day: "2-digit",
+      month: "short",
+      year: "2-digit",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    };
+
+    //const formattedDate = new Date(isoDate).toLocaleTimeString([],options)
+
+    const formattedDate = date.toLocaleDateString("en-IN", options);
+    //const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
+    return formattedDate;
+  };
+
   return (
     <div className="home-container">
       <div className="button-container">
@@ -64,7 +87,7 @@ const HomePage = () => {
             handleOpenLink("linkedin");
           }}
         >
-          View Navaneeth's Profile
+          Navaneeth's Profile
         </button>
         <button className="home-button" onClick={handleLogout}>
           Logout
@@ -125,6 +148,267 @@ const HomePage = () => {
             ))}
           </ul>
         </div>
+      </div>
+
+      {verificationHistory && Object.keys(verificationHistory).length > 1 && (
+        <h3 className="result-text">
+          <span className="name-list">
+            {" "}
+            User Verification and Login History
+          </span>{" "}
+        </h3>
+      )}
+      <div className="desktop-table">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            margin: "0 40px",
+          }}
+        >
+          {verificationHistory &&
+            Object.keys(verificationHistory).length > 1 && (
+              <table style={{ border: "1px solid black", width: "auto" }}>
+                <thead>
+                  <tr>
+                    <th style={{ border: "1px solid black" }}>Event</th>
+                    <th style={{ border: "1px solid black" }}>SMS Status</th>
+                    <th style={{ border: "1px solid black" }}>SMS Sent At</th>
+                    <th style={{ border: "1px solid black" }}>
+                      Verification Attempts
+                    </th>
+                    <th style={{ border: "1px solid black" }}>
+                      Verification Status
+                    </th>
+                    <th style={{ border: "1px solid black" }}>Verified At</th>
+                    <th style={{ border: "1px solid black" }}>Logged Out At</th>
+                    <th style={{ border: "1px solid black" }}>
+                      Logs / Actions
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.values(verificationHistory).reverse().map((verification) => (
+                    <tr key={verification.id}>
+                      <td
+                        style={{
+                          border: "1px solid black",
+                          textAlign: "center",
+                        }}
+                      >
+                        {verification.code_type}
+                      </td>
+                      <td
+                        style={{
+                          border: "1px solid black",
+                          textAlign: "center",
+                        }}
+                      >
+                        {verification.sentStatus.toString()}
+                      </td>
+                      <td
+                        style={{
+                          border: "1px solid black",
+                          textAlign: "center",
+                        }}
+                      >
+                        {convertDate(verification.createdAt)}
+                      </td>
+                      <td
+                        style={{
+                          border: "1px solid black",
+                          textAlign: "center",
+                        }}
+                      >
+                        {verification.attempts}
+                      </td>
+                      <td
+                        style={{
+                          border: "1px solid black",
+                          textAlign: "center",
+                        }}
+                      >
+                        {verification.isVerified.toString()}
+                      </td>
+                      <td
+                        style={{
+                          border: "1px solid black",
+                          textAlign: "center",
+                        }}
+                      >
+                        {convertDate(verification.verifiedAt)}
+                      </td>
+                      <td
+                        style={{
+                          border: "1px solid black",
+                          textAlign: "center",
+                        }}
+                      >
+                        {convertDate(verification.loggedOutAt)}
+                      </td>
+                      <td
+                        style={{
+                          border: "1px solid black",
+                          textAlign: "center",
+                        }}
+                      >
+                        {verification.status}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
+        </div>
+      </div>
+
+      <div
+        className="mobile-table"
+        style={{
+        }}
+      >
+        <table
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            border: "1px solid black",
+            width: "80%",
+            marginBottom: "10px 10px",
+            tableLayout: "fixed",
+          }}
+        >
+          <tbody>
+            {Object.values(verificationHistory).reverse().map((verification, index) => (
+              <div>
+                <tr key={index} style={{ flex: 1 }}>
+                  <th
+                    style={{
+                      border: "1px solid black",
+
+                      textAlign: "center",
+                      width: "30%",
+                    }}
+                  >
+                    Event
+                  </th>
+                  <td
+                    style={{ border: "1px solid black", textAlign: "center" }}
+                  >
+                    {verification.code_type}
+                  </td>
+                </tr>
+                <tr style={{ flex: 1 }}>
+                  <th
+                    style={{
+                      border: "1px solid black",
+
+                      textAlign: "center",
+                      width: "30%",
+                    }}
+                  >
+                    SMS Status
+                  </th>
+                  <td
+                    style={{ border: "1px solid black", textAlign: "center" }}
+                  >
+                    {convertDate(verification.createdAt)}
+                  </td>
+                </tr>
+                <tr>
+                  <th
+                    style={{
+                      border: "1px solid black",
+
+                      textAlign: "center",
+                      width: "30%",
+                    }}
+                  >
+                    Verification Attempts
+                  </th>
+                  <td
+                    style={{ border: "1px solid black", textAlign: "center" }}
+                  >
+                    {verification.attempts}
+                  </td>
+                </tr>
+                <tr>
+                  <th
+                    style={{
+                      border: "1px solid black",
+
+                      textAlign: "center",
+                      width: "30%",
+                    }}
+                  >
+                    Verification Status
+                  </th>
+                  <td
+                    style={{ border: "1px solid black", textAlign: "center" }}
+                  >
+                    {verification.isVerified.toString()}
+                  </td>
+                </tr>
+
+                <tr>
+                  <th
+                    style={{
+                      border: "1px solid black",
+
+                      textAlign: "center",
+                      width: "30%",
+                    }}
+                  >
+                    Verified At
+                  </th>
+                  <td
+                    style={{ border: "1px solid black", textAlign: "center" }}
+                  >
+                    {convertDate(verification.verifiedAt)}
+                  </td>
+                </tr>
+
+                <tr>
+                  <th
+                    style={{
+                      border: "1px solid black",
+
+                      textAlign: "center",
+                      width: "30%",
+                    }}
+                  >
+                    Logged Out At
+                  </th>
+                  <td
+                    style={{ border: "1px solid black", textAlign: "center" }}
+                  >
+                    {convertDate(verification.loggedOutAt)}
+                  </td>
+                </tr>
+
+                <tr>
+                  <th
+                    style={{
+                      border: "1px solid black",
+
+                      textAlign: "center",
+                      width: "30%",
+                    }}
+                  >
+                    Logs / Actions
+                  </th>
+                  <td
+                    style={{ border: "1px solid black", textAlign: "center" }}
+                  >
+                    {verification.status}
+                  </td>
+                </tr>
+                <div style={{ marginTop: "10px" }}></div>
+              </div>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
